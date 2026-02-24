262ccc628142656a0300f8ffeb96da428299e232
waltertaya <waltermittybecker5@gmail.com>
2026-02-24 19:26:32 +0300

    prefixed endpoints to /api/v1

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\WalletController;
+use App\Http\Controllers\Api\TransactionController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/api/v1')->group(function () {
+    // User routes
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+
+    // Wallet routes
+    Route::post('/wallets', [WalletController::class, 'store']);
+    Route::get('/wallets/{wallet}', [WalletController::class, 'show']);
+    Route::put('/wallets/{wallet}', [WalletController::class, 'update']);
+    Route::delete('/wallets/{wallet}', [WalletController::class, 'destroy']);
+
+    // Transaction routes
+    Route::post('/wallets/{wallet}/transactions', [TransactionController::class, 'store']);
+    Route::get('/wallets/{wallet}/transactions', [TransactionController::class, 'index']);
+    Route::get('/transactions/{transaction}', [TransactionController::class, 'show']);
+});
